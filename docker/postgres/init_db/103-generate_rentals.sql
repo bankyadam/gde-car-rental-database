af5a0843cf897c8428287bc2a0e4f3dcec5c2caf
Adam Banky <banky.adam@gmail.com>
2023-11-07 14:25:33 +0100

    Query formatting

--- docker/postgres/init_db/103-generate_rentals.sql
+++ docker/postgres/init_db/103-generate_rentals.sql
@@ -4,18 +4,18 @@ INSERT INTO
     SELECT
         customer_id,
         car_vin,
-        start_date::date AS start_date,
-        (start_date + RANDOM() * INTERVAL '30 days')::date AS end_date
+        start_date::DATE AS start_date,
+        (start_date + RANDOM() * INTERVAL '30 days')::DATE AS end_date
     FROM (
         SELECT
             customers.id AS customer_id,
             cars.vin AS car_vin,
             CURRENT_DATE - INTERVAL '3 years' + RANDOM() * INTERVAL '3 years' AS start_date
         FROM (
-            SELECT id, ROW_NUMBER() OVER(ORDER BY RANDOM()) rn FROM customers
+            SELECT id, ROW_NUMBER() OVER(ORDER BY RANDOM()) r FROM customers
         ) customers
         INNER JOIN (
-            SELECT vin, ROW_NUMBER() OVER(ORDER BY RANDOM()) rn FROM cars
-        ) cars ON cars.rn = customers.rn
-        WHERE customers.rn <= 300
+            SELECT vin, ROW_NUMBER() OVER(ORDER BY RANDOM()) r FROM cars
+        ) cars ON cars.r = customers.r
+        WHERE customers.r <= 300
     ) rental_partial
