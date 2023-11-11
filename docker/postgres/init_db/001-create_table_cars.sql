5f64d38dda64269eead0861f9a66ab08664cbf59
Adam Banky <banky.adam@gmail.com>
2023-11-12 00:27:30 +0100

    table refactor cars and introduce status

--- docker/postgres/init_db/001-create_table_cars.sql
+++ docker/postgres/init_db/001-create_table_cars.sql
@@ -1,12 +1,15 @@
+CREATE TYPE car_status AS ENUM ('rentable', 'repairing', 'inactive', 'rented', 'cleaning');
+
 CREATE TABLE IF NOT EXISTS cars
 (
-    vin                        TEXT    NOT NULL,
-    manufacturer               TEXT    NOT NULL,
-    model                      TEXT    NOT NULL,
-    model_year                 DATE    NOT NULL,
-    color                      TEXT    NOT NULL,
-    vehicle_inspection_duedate DATE    NOT NULL,
-    price                      INTEGER NOT NULL,
-    CONSTRAINT cars_vin_pk
-        PRIMARY KEY (vin)
+    vin                        TEXT                                      NOT NULL,
+    manufacturer               TEXT                                      NOT NULL,
+    model                      TEXT                                      NOT NULL,
+    model_year                 DATE                                      NOT NULL,
+    color                      TEXT                                      NOT NULL,
+    vehicle_inspection_duedate DATE                                      NOT NULL,
+    net_price_daily            INTEGER                                   NOT NULL,
+    status                     car_status DEFAULT 'rentable'::car_status NOT NULL,
+    CONSTRAINT cars_vin_pk PRIMARY KEY (vin)
 );
+
